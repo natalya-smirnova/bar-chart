9ca7d2cd67deb58aa76c8781c9ee84526d48e47e
natalya-smirnova <v-nsmirnova@microsoft.com>
2021-11-26 20:38:59 +0300

    add test selection

--- src/barChart.ts
+++ src/barChart.ts
@@ -1,4 +1,4 @@
-// import "./../style/visual.less";
+import "./../style/visual.less";
 import {
     event as d3Event,
     select as d3Select
@@ -15,6 +15,8 @@ import "regenerator-runtime/runtime";
 import powerbi = powerbiVisualsApi;
 
 type Selection<T1, T2 = T1> = d3.Selection<any, T1, any, T2>;
+import ScaleLinear = d3.ScaleLinear;
+const getEvent = () => require("d3-selection").event;
 
 // powerbi.visuals
 import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
@@ -23,20 +25,22 @@ import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInst
 import Fill = powerbi.Fill;
 import ISandboxExtendedColorPalette = powerbi.extensibility.ISandboxExtendedColorPalette;
 import ISelectionId = powerbi.visuals.ISelectionId;
+import ISelectionManager = powerbi.extensibility.ISelectionManager;
 import IVisual = powerbi.extensibility.IVisual;
 import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import PrimitiveValue = powerbi.PrimitiveValue;
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualEnumerationInstanceKinds = powerbi.VisualEnumerationInstanceKinds;
-import ISelectionManager = powerbi.extensibility.ISelectionManager;
 
-import * as tms from "powerbi-visuals-utils-formattingutils";
-import textMeasurementService = tms.textMeasurementService;
+import {createTooltipServiceWrapper, ITooltipServiceWrapper} from "powerbi-visuals-utils-tooltiputils";
+import { textMeasurementService } from "powerbi-visuals-utils-formattingutils";
 
 import { getValue, getCategoricalObjectValue } from "./objectEnumerationUtility";
+// import { getLocalizedString } from "./localization/localizationHelper"
 import { dataViewWildcard } from "powerbi-visuals-utils-dataviewutils";
 
 /**
@@ -78,7 +82,7 @@ interface BarChartDataPoint {
  * @property {{show:boolean}} enableAxis - Object property that allows axis to be enabled.
  * @property {{generalView.opacity:number}} Bars Opacity - Controls opacity of plotted bars, values range between 10 (almost transparent) to 100 (fully opaque, default)
  * @property {{generalView.showHelpLink:boolean}} Show Help Button - When TRUE, the plot displays a button which launch a link to documentation.
-*/
+ */
 interface BarChartSettings {
     enableAxis: {
         show: boolean;
@@ -89,6 +93,13 @@ interface BarChartSettings {
         opacity: number;
         showHelpLink: boolean;
         helpLinkColor: string;
+    };
+
+    averageLine: {
+        show: boolean;
+        displayName: string;
+        fill: string;
+        showDataLabel: boolean;
     };
 }
 
@@ -101,6 +112,12 @@ let defaultSettings: BarChartSettings = {
         opacity: 100,
         showHelpLink: false,
         helpLinkColor: "#80B0E0",
+    },
+    averageLine: {
+        show: false,
+        displayName: "Average Line",
+        fill: "#888888",
+        showDataLabel: false
     }
 };
 
@@ -152,7 +169,13 @@ function visualTransform(options: VisualUpdateOptions, host: IVisualHost): BarCh
             opacity: getValue<number>(objects, 'generalView', 'opacity', defaultSettings.generalView.opacity),
             showHelpLink: getValue<boolean>(objects, 'generalView', 'showHelpLink', defaultSettings.generalView.showHelpLink),
             helpLinkColor: strokeColor,
-        }
+        },
+        averageLine: {
+            show: getValue<boolean>(objects, 'averageLine', 'show', defaultSettings.averageLine.show),
+            displayName: getValue<string>(objects, 'averageLine', 'displayName', defaultSettings.averageLine.displayName),
+            fill: getValue<string>(objects, 'averageLine', 'fill', defaultSettings.averageLine.fill),
+            showDataLabel: getValue<boolean>(objects, 'averageLine', 'showDataLabel', defaultSettings.averageLine.showDataLabel),
+        },
     };
 
     const strokeWidth: number = getColumnStrokeWidth(colorPalette.isHighContrast);
@@ -243,19 +266,26 @@ function getAxisTextFillColor(
 export class BarChart implements IVisual {
     private svg: Selection<any>;
     private host: IVisualHost;
+    private selectionManager: ISelectionManager;
     private barContainer: Selection<SVGElement>;
     private xAxis: Selection<SVGElement>;
     private barDataPoints: BarChartDataPoint[];
     private barChartSettings: BarChartSettings;
-    private selectionManager: ISelectionManager;
+    private tooltipServiceWrapper: ITooltipServiceWrapper;
+    private locale: string;
     private helpLinkElement: Selection<any>;
+    private element: HTMLElement;
+    private isLandingPageOn: boolean;
+    private LandingPageRemoved: boolean;
+    private LandingPage: Selection<any>;
+    private averageLine: Selection<SVGElement>;
 
     private barSelection: d3.Selection<d3.BaseType, any, d3.BaseType, any>;
 
     static Config = {
         xScalePadding: 0.1,
         solidOpacity: 1,
-        transparentOpacity: 1,
+        transparentOpacity: 0.4,
         margins: {
             top: 0,
             right: 0,
@@ -275,11 +305,15 @@ export class BarChart implements IVisual {
      */
     constructor(options: VisualConstructorOptions) {
         this.host = options.host;
+        this.element = options.element;
         this.selectionManager = options.host.createSelectionManager();
+        this.locale = options.host.locale;
 
         this.selectionManager.registerOnSelectCallback(() => {
             this.syncSelectionState(this.barSelection, <ISelectionId[]>this.selectionManager.getSelectionIds());
         });
+
+        this.tooltipServiceWrapper = createTooltipServiceWrapper(this.host.tooltipService, options.element);
 
         this.svg = d3Select(options.element)
             .append('svg')
@@ -293,10 +327,14 @@ export class BarChart implements IVisual {
             .append('g')
             .classed('xAxis', true);
 
-            const helpLinkElement: Element = this.createHelpLinkElement();
-            options.element.appendChild(helpLinkElement);
-    
-            this.helpLinkElement = d3Select(helpLinkElement);
+        this.initAverageLine();
+
+        const helpLinkElement: Element = this.createHelpLinkElement();
+        options.element.appendChild(helpLinkElement);
+
+        this.helpLinkElement = d3Select(helpLinkElement);
+
+        this.handleContextMenu();
     }
 
     /**
@@ -311,7 +349,8 @@ export class BarChart implements IVisual {
         let viewModel: BarChartViewModel = visualTransform(options, this.host);
         let settings = this.barChartSettings = viewModel.settings;
         this.barDataPoints = viewModel.dataPoints;
-
+        // Turn on landing page in capabilities and remove comment to turn on landing page!
+        // this.HandleLandingPage(options);
         let width = options.viewport.width;
         let height = options.viewport.height;
 
@@ -323,11 +362,11 @@ export class BarChart implements IVisual {
             let margins = BarChart.Config.margins;
             height -= margins.bottom;
         }
-        
+
         this.helpLinkElement
-        .classed("hidden", !settings.generalView.showHelpLink)
-        .style("border-color", settings.generalView.helpLinkColor)
-        .style("color", settings.generalView.helpLinkColor);
+            .classed("hidden", !settings.generalView.showHelpLink)
+            .style("border-color", settings.generalView.helpLinkColor)
+            .style("color", settings.generalView.helpLinkColor);
 
         this.xAxis
             .style("font-size", Math.min(height, width) * BarChart.Config.xAxisFontMultiplier)
@@ -343,7 +382,6 @@ export class BarChart implements IVisual {
             .padding(0.2);
 
         let xAxis = axisBottom(xScale);
-
         const colorObjects = options.dataViews[0] ? options.dataViews[0].metadata.objects : null;
         this.xAxis.attr('transform', 'translate(0, ' + height + ')')
             .call(xAxis)
@@ -355,6 +393,7 @@ export class BarChart implements IVisual {
 
         const textNodes = this.xAxis.selectAll("text")
         BarChart.wordBreak(textNodes, xScale.bandwidth(), height);
+        this.handleAverageLineUpdate(height, width, yScale);
 
         this.barSelection = this.barContainer
             .selectAll('.bar')
@@ -366,7 +405,7 @@ export class BarChart implements IVisual {
             .merge(<any>this.barSelection);
 
         barSelectionMerged.classed('bar', true);
-        
+
         const opacity: number = viewModel.settings.generalView.opacity / 100;
         barSelectionMerged
             .attr("width", xScale.bandwidth())
@@ -379,7 +418,11 @@ export class BarChart implements IVisual {
             .style("stroke", (dataPoint: BarChartDataPoint) => dataPoint.strokeColor)
             .style("stroke-width", (dataPoint: BarChartDataPoint) => `${dataPoint.strokeWidth}px`);
 
-        
+        this.tooltipServiceWrapper.addTooltip(barSelectionMerged,
+            (datapoint: BarChartDataPoint) => this.getTooltipData(datapoint),
+            (datapoint: BarChartDataPoint) => datapoint.selectionId
+        );
+
         this.syncSelectionState(
             barSelectionMerged,
             <ISelectionId[]>this.selectionManager.getSelectionIds()
@@ -394,28 +437,27 @@ export class BarChart implements IVisual {
                     .then((ids: ISelectionId[]) => {
                         this.syncSelectionState(barSelectionMerged, ids);
                     });
-
                 (<Event>d3Event).stopPropagation();
             }
         });
-
         this.barSelection
             .exit()
             .remove();
-        
         this.handleClick(barSelectionMerged);
     }
 
-    private createHelpLinkElement(): Element {
-        let linkElement = document.createElement("a");
-        linkElement.textContent = "?";
-        linkElement.setAttribute("title", "Open documentation");
-        linkElement.setAttribute("class", "helpLink");
-        linkElement.addEventListener("click", () => {
-            this.host.launchUrl("https://microsoft.github.io/PowerBI-visuals/tutorials/building-bar-chart/adding-url-launcher-element-to-the-bar-chart/");
+    private static wordBreak(
+        textNodes: Selection<any, SVGElement>,
+        allowedWidth: number,
+        maxHeight: number
+    ) {
+        textNodes.each(function () {
+            textMeasurementService.wordBreak(
+                this,
+                allowedWidth,
+                maxHeight);
         });
-        return linkElement;
-    };
+    }
 
     private handleClick(barSelection: d3.Selection<d3.BaseType, any, d3.BaseType, any>) {
         // Clear selection when clicking outside a bar
@@ -430,16 +472,16 @@ export class BarChart implements IVisual {
         });
     }
 
-    private static wordBreak(
-        textNodes: Selection<any, SVGElement>,
-        allowedWidth: number,
-        maxHeight: number
-    ) {
-        textNodes.each(function () {
-            textMeasurementService.wordBreak(
-                this,
-                allowedWidth,
-                maxHeight);
+    private handleContextMenu() {
+        this.svg.on('contextmenu', () => {​​
+            const mouseEvent: MouseEvent = getEvent();
+            const eventTarget: EventTarget = mouseEvent.target;
+            let dataPoint: any = d3Select(<d3.BaseType>eventTarget).datum();
+            this.selectionManager.showContextMenu(dataPoint ? dataPoint.selectionId : {}, {
+                x: mouseEvent.clientX,
+                y: mouseEvent.clientY
+            });
+            mouseEvent.preventDefault();
         });
     }
 
@@ -531,7 +573,6 @@ export class BarChart implements IVisual {
                     });
                 }
                 break;
-            
             case 'generalView':
                 objectEnumeration.push({
                     objectName: objectName,
@@ -550,8 +591,154 @@ export class BarChart implements IVisual {
                     selector: null
                 });
                 break;
+            case 'averageLine':
+                objectEnumeration.push({
+                    objectName: objectName,
+                    properties: {
+                        show: this.barChartSettings.averageLine.show,
+                        displayName: this.barChartSettings.averageLine.displayName,
+                        fill: this.barChartSettings.averageLine.fill,
+                        showDataLabel: this.barChartSettings.averageLine.showDataLabel
+                    },
+                    selector: null
+                });
+                break;
         };
 
         return objectEnumeration;
+    }
+
+    /**
+     * Destroy runs when the visual is removed. Any cleanup that the visual needs to
+     * do should be done here.
+     *
+     * @function
+     */
+    public destroy(): void {
+        // Perform any cleanup tasks here
+    }
+
+    private getTooltipData(value: any): VisualTooltipDataItem[] {
+        //let language = getLocalizedString(this.locale, "LanguageKey");
+        return [{
+            displayName: value.category,
+            value: value.value.toString(),
+            color: value.color,
+            header: "Just header"
+        }];
+    }
+
+    private createHelpLinkElement(): Element {
+        let linkElement = document.createElement("a");
+        linkElement.textContent = "?";
+        linkElement.setAttribute("title", "Open documentation");
+        linkElement.setAttribute("class", "helpLink");
+        linkElement.addEventListener("click", () => {
+            this.host.launchUrl("https://microsoft.github.io/PowerBI-visuals/tutorials/building-bar-chart/adding-url-launcher-element-to-the-bar-chart/");
+        });
+        return linkElement;
+    };
+
+    private handleLandingPage(options: VisualUpdateOptions) {
+        if (!options.dataViews || !options.dataViews.length) {
+            if (!this.isLandingPageOn) {
+                this.isLandingPageOn = true;
+                const SampleLandingPage: Element = this.createSampleLandingPage();
+                this.element.appendChild(SampleLandingPage);
+
+                this.LandingPage = d3Select(SampleLandingPage);
+            }
+
+        } else {
+            if (this.isLandingPageOn && !this.LandingPageRemoved) {
+                this.LandingPageRemoved = true;
+                this.LandingPage.remove();
+            }
+        }
+    }
+
+    private createSampleLandingPage(): Element {
+        let div = document.createElement("div");
+
+        let header = document.createElement("h1");
+        header.textContent = "Sample Bar Chart Landing Page";
+        header.setAttribute("class", "LandingPage");
+        let p1 = document.createElement("a");
+        p1.setAttribute("class", "LandingPageHelpLink");
+        p1.textContent = "Learn more about Landing page";
+
+        p1.addEventListener("click", () => {
+            this.host.launchUrl("https://microsoft.github.io/PowerBI-visuals/docs/overview/");
+        });
+
+        div.appendChild(header);
+        div.appendChild(p1);
+
+        return div;
+    }
+
+    private getColorValue(color: Fill | string): string {
+        // Override color settings if in high contrast mode
+        if (this.host.colorPalette.isHighContrast) {
+            return this.host.colorPalette.foreground.value;
+        }
+
+        // If plain string, just return it
+        if (typeof (color) === 'string') {
+            return color;
+        }
+        // Otherwise, extract string representation from Fill type object
+        return color.solid.color;
+    }
+
+    private initAverageLine() {
+        this.averageLine = this.svg
+            .append('g')
+            .classed('averageLine', true);
+
+        this.averageLine.append('line')
+            .attr('id', 'averageLine');
+
+        this.averageLine.append('text')
+            .attr('id', 'averageLineLabel');
+    }
+
+    private handleAverageLineUpdate(height: number, width: number, yScale: ScaleLinear<number, number>) {
+        let average = this.calculateAverage();
+        let fontSize = Math.min(height, width) * BarChart.Config.xAxisFontMultiplier;
+        let chosenColor = this.getColorValue(this.barChartSettings.averageLine.fill);
+        // If there's no room to place lable above line, place it below
+        let labelYOffset = fontSize * ((yScale(average) > fontSize * 1.5) ? -0.5 : 1.5);
+
+        this.averageLine
+            .style("font-size", fontSize)
+            .style("display", (this.barChartSettings.averageLine.show) ? "initial" : "none")
+            .attr("transform", "translate(0, " + Math.round(yScale(average)) + ")");
+
+        this.averageLine.select("#averageLine")
+            .style("stroke", chosenColor)
+            .style("stroke-width", "3px")
+            .style("stroke-dasharray", "6,6")
+            .attr("x1", 0)
+            .attr("x1", "" + width);
+
+        this.averageLine.select("#averageLineLabel")
+            .text("Average: " + average.toFixed(2))
+            .attr("transform", "translate(0, " + labelYOffset + ")")
+            .style("fill", this.barChartSettings.averageLine.showDataLabel ? chosenColor : "none");
+    }
+
+    private calculateAverage(): number {
+        if (this.barDataPoints.length === 0) {
+            return 0;
+        }
+
+        let total = 0;
+
+        this.barDataPoints.forEach((value: BarChartDataPoint) => {
+            total += <number>value.value;
+        });
+
+        return total / this.barDataPoints.length;
     }
 }
